d303f4dfcd2259a8fac2726278db7e3ea55de4e4
Shainu Suhas <shainusuhas7@gmail.com>
2025-08-01 21:21:03 +0530

    folder structure add aeyindi

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'cellSegmentation',
+    version= '0.0.0',
+    author= 'D Shainu suhas',
+    author_email= 'shainusuhas7@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
